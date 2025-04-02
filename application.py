7ab226cbc75b4a24494c1095c27961ff53f60a44
Ajaykapratwar <156508901+Ajaykapratwar@users.noreply.github.com>
2025-04-02 10:08:54 +0530

    Update application.py

--- application.py
+++ application.py
@@ -8,8 +8,8 @@ application = Flask(__name__)
 app = application
 
 # import ridge regressor and standard scaler pickle
-ridge_model = pickle.load(open('E:\\ML\\Step By Step Project Implementation\\forest-fire\\models\\ridge.pkl', 'rb'))
-standard_scaler = pickle.load(open('E:\\ML\\Step By Step Project Implementation\\forest-fire\\models\\scaler.pkl', 'rb'))
+ridge_model = pickle.load(open('models\\ridge.pkl', 'rb'))
+standard_scaler = pickle.load(open('models\\scaler.pkl', 'rb'))
 
 @app.route('/')
 def index():
